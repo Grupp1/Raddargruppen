fe7bf2f9596f00db5b680cce21ab3a46977bb31a
Daniel Andersson <danan612@student.liu.se>
2011-10-21 11:49:59 +0200

    hej

--- Raddargruppen/src/raddar/views/StartView.java
+++ Raddargruppen/src/raddar/views/StartView.java
@@ -5,22 +5,35 @@ import raddar.gruppen.R;
 import raddar.models.TextMessage;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class StartView extends Activity {
+public class StartView extends Activity implements OnClickListener {
 
+	private Button loginButton;
+	private EditText user;
+	private EditText password;
+	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+		setContentView(R.layout.start);
+		loginButton = (Button)this.findViewById(R.id.button2);
+		loginButton.setOnClickListener(this);
+		user = (EditText)this.findViewById(R.id.editText1);
+		password = (EditText)this.findViewById(R.id.editText2);
+		
 
+	}
 
-
-		//TextMessage tm = new TextMessage(MessageType.TEXT, "Borche", "Alice");
-		//tm.setMessage("Hejsan. Detta är ett INTEFFE testmeddelande!");
-		//TextView tv = (TextView) findViewById(R.id.textView1);
-		//tv.setText(tm.getFormattedMessage());
+	public void onClick(View v) {
+		//anropar loginfunktionen - går vidare till main
+		finish();
+		
 	}
 
 }
