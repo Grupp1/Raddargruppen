e995ba51c070571d6a9d266bbd6906ed73a29176
Niklas Lavrell <niklas.lavrell@gmail.com>
2011-10-21 11:51:23 +0200

    kartfunktionen

--- Raddargruppen/src/raddar/models/resource.java
+++ Raddargruppen/src/raddar/models/resource.java
@@ -1,8 +1,14 @@
 package raddar.models;
 
+import android.graphics.drawable.Drawable;
 import raddar.enums.ResourceStatus;
 
 public class Resource extends MapObject {
+
+	public Resource(Drawable defaultMarker) {
+		super(defaultMarker);
+		// TODO Auto-generated constructor stub
+	}
 
 	private ResourceStatus status;
 
--- Raddargruppen/src/raddar/models/situation.java
+++ Raddargruppen/src/raddar/models/situation.java
@@ -1,9 +1,14 @@
 package raddar.models;
 
+import android.graphics.drawable.Drawable;
 import raddar.enums.SituationPriority;
 
 public class Situation extends MapObject {
 
+	public Situation(Drawable defaultMarker) {
+		super(defaultMarker);
+		// TODO Auto-generated constructor stub
+	}
 	private String description;
 	private SituationPriority priority;
 	
