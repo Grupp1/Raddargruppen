04081ef84273495cfd4f03e76344e6ca7dc27ec8
Andreas Borjesson <borjesson.andreas@gmail.com>
2011-10-24 10:27:57 +0200

    Andrade ordning pa prioritet och type sa att det passar servern

--- Raddargruppen/src/raddar/models/TextMessage.java
+++ Raddargruppen/src/raddar/models/TextMessage.java
@@ -28,6 +28,10 @@ public class TextMessage extends Message {
 		this.data = message;
 	}
 	
+	public void appendMessage(String message) {
+		this.data += message;
+	}
+	
 	public String getMessage() {
 		return data;
 	}
@@ -39,11 +43,11 @@ public class TextMessage extends Message {
 	@Override
 	public String toString() {
 		String formattedMessage = "";
-		formattedMessage += Message.HEADER_PRIO + priority +"\r\n";
-		formattedMessage += Message.HEADER_TYPE + type + "\r\n";
-		formattedMessage += Message.HEADER_FROM + fromUser.toLowerCase() + "\r\n";
-		formattedMessage += Message.HEADER_TO + toUser.toLowerCase() + "\r\n";
-		formattedMessage += "\r\n\r\n";
+		formattedMessage += Message.HEADER_TYPE + type + CRLF;
+		formattedMessage += Message.HEADER_PRIO + priority + CRLF;
+		formattedMessage += Message.HEADER_FROM + fromUser.toLowerCase() + CRLF;
+		formattedMessage += Message.HEADER_TO + toUser.toLowerCase() + CRLF;
+		formattedMessage += CRLF + CRLF;
 		formattedMessage += data;
 		
 		return formattedMessage;
