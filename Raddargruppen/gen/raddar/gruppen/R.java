0b1580030c9de0555fd8acf1d7bea17dd7956cc1
Niklas Lavrell <niklas.lavrell@gmail.com>
2011-11-01 16:46:21 +0100

    map

--- Raddargruppen/gen/raddar/gruppen/R.java
+++ Raddargruppen/gen/raddar/gruppen/R.java
@@ -11,10 +11,11 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
-        public static final int magnus=0x7f020001;
-        public static final int niklas=0x7f020002;
-        public static final int resource=0x7f020003;
+        public static final int fire=0x7f020000;
+        public static final int icon=0x7f020001;
+        public static final int magnus=0x7f020002;
+        public static final int niklas=0x7f020003;
+        public static final int resource=0x7f020004;
     }
     public static final class id {
         public static final int button1=0x7f050001;
