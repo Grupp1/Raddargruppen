8af28f74e46f0e2b086a75fbb5df4141be1c1739
Marcus Einar <marcus.einar@gmail.com>
2011-11-03 10:57:44 +0100

    111103 1057

--- Raddargruppen/src/raddar/models/ClientDatabase.java
+++ Raddargruppen/src/raddar/models/ClientDatabase.java
@@ -5,14 +5,26 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class ClientDatabase extends SQLiteOpenHelper{
-	private static final int DATABASE_VERSION = 2;
+public class ClientDatabase extends SQLiteOpenHelper implements Runnable{
+	private static final int DATABASE_VERSION = 1;
 	private static final String DICTIONARY_TABLE_NAME = "Inbox";
-	//DEFINIERA HUR DE SKA SE UT
+	//Skapa databasen "inbox" statement
+	private static final String INBOX_CREATE = "create table inbox (from VARCHAR(20), to " +
+			"VARCHAR(20), recieved-at DATETIME, subject VARCHAR(20), prioroty VARCHAR(5)";
+	
+	
+	
+	
+	
+	
+	
+	/*	//DEFINIERA HUR DE SKA SE UT
 	private static final String DICTIONARY_TABLE_CREATE =
 			"CREATE TABLE " + DICTIONARY_TABLE_NAME + " (" +
 					"KEY_WORD" + " TEXT, " +
 					"KEY_DEFINITION" + " TEXT);";
+*/
+	
 	
 	public ClientDatabase(Context context) {
 		super(context, DICTIONARY_TABLE_NAME, null, DATABASE_VERSION);
@@ -21,12 +33,17 @@ public class ClientDatabase extends SQLiteOpenHelper{
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		// TODO Auto-generated method stub
+		db.execSQL(INBOX_CREATE);
 		
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void run() {
 		// TODO Auto-generated method stub
 		
 	}
