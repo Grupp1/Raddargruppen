b04426caaa288218c9120b7c75dd0aa72c385191
Andreas Borjesson <borjesson.andreas@gmail.com>
2011-11-10 13:30:34 +0100

    NotificationMessage.java

--- TDDD36Server/src/tddd36/server/NotificationMessage.java
+++ TDDD36Server/src/tddd36/server/NotificationMessage.java
@@ -4,23 +4,23 @@ import raddar.enums.MessageType;
 import raddar.enums.NotificationType;
 
 public class NotificationMessage extends Message {
-	
+
 	// Message-header attributen för NotificationMessages
 	public final static String HEADER_NOTIFICATION = "Notification: ";
-	
+
 	private NotificationType notification;
-	
+
 	public NotificationMessage(String fromUser, NotificationType notification) {
 		this.fromUser = fromUser;
 		this.notification = notification;
 		this.type = MessageType.NOTIFICATION;
 	}
-	
+
 	@Override
 	public String getFormattedMessage() {
 		return toString();
 	}
-	
+
 	public NotificationType getNotification() {
 		return notification;
 	}
@@ -34,9 +34,9 @@ public class NotificationMessage extends Message {
 		String formattedMessage = "";
 		formattedMessage += HEADER_TYPE + type.toString() + CRLF;
 		formattedMessage += HEADER_FROM + fromUser + CRLF;
-		formattedMessage += HEADER_NOTIFICATION + notification.toString() + CRLF;
-		
+		formattedMessage += HEADER_NOTIFICATION + notification.toString()
+				+ CRLF;
+
 		return formattedMessage;
 	}
 }
-
