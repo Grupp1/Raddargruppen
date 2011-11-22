6bc66c37916ba0078bc2b21cb5e9f211f84e651d
Magnus Kjellander <magkj501@student.liu.se>
2011-11-22 08:46:42 +0100

    pullat

--- Raddargruppen/src/raddar/views/WeatherView.java
+++ Raddargruppen/src/raddar/views/WeatherView.java
@@ -5,6 +5,8 @@ import java.util.Date;
 
 import android.app.ExpandableListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.ExpandableListAdapter;
 
@@ -32,6 +34,58 @@ public class WeatherView extends ExpandableListActivity {
         	SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         	return sdf.format(date);
         }
+
+		public Object getChild(int groupPosition, int childPosition) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public long getChildId(int groupPosition, int childPosition) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		public View getChildView(int groupPosition, int childPosition,
+				boolean isLastChild, View convertView, ViewGroup parent) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public int getChildrenCount(int groupPosition) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		public Object getGroup(int groupPosition) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public int getGroupCount() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		public long getGroupId(int groupPosition) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		public View getGroupView(int groupPosition, boolean isExpanded,
+				View convertView, ViewGroup parent) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public boolean hasStableIds() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		public boolean isChildSelectable(int groupPosition, int childPosition) {
+			// TODO Auto-generated method stub
+			return false;
+		}
         
     }
     
