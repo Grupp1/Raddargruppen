3a7f55b4caf6b25f371a84e693c4f9a2bb1938f7
Magnus Kjellander <magkj501@student.liu.se>
2011-11-23 14:00:34 +0100

    merging m master

--- TDDD36Server/src/tddd36/server/Database.java
+++ TDDD36Server/src/tddd36/server/Database.java
@@ -2,13 +2,16 @@ package tddd36.server;
 
 
 
-import java.awt.TrayIcon.MessageType;
+import raddar.enums.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+
+import raddar.models.Encryption;
+import raddar.models.TextMessage;
 
 
 /**
